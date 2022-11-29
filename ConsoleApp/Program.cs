ff38520b71374eaf9fafc3cf6fedb8365863f274
admire114su@gmail.com <admire114su@gmail.com>
2022-11-30 00:02:29 +0500

    2 exercises

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,13 +10,65 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            //Longest Common Prefix
+            string[] words = new string[3] { "flower", "flight", "flag" };
+            string str = LongestCommonPrefix(words);
+            Console.WriteLine(str);
 
+            //Valid Parentheses
+            string str1 = "({{}})";
+            if(IsValid(str1))
+                Console.WriteLine("RIGHT!");
+            else Console.WriteLine("ERROR!");
+
+            Console.ReadLine();
         }
 
         //Longest Common Prefix
         static string LongestCommonPrefix(string[] strs)
         {
-            return "";
+            if (strs.Length > 1)
+            {
+                int prefixLength = 0;
+
+                foreach (char c in strs[0])
+                {
+                    foreach (string s in strs)
+                    {
+                        if (s.Length <= prefixLength || s[prefixLength] != c)
+                        {
+                            return strs[0].Substring(0, prefixLength);
+                        }
+                    }
+                    prefixLength++;
+                }
+            }
+            return strs[0];           
+        }
+
+        //Valid Parentheses
+        static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in s)
+            {
+                if (ch == ')' || ch == '}' || ch == ']')
+                {
+                    if (stack.Count > 0)
+                    {
+                        char chSt = stack.Pop();
+
+                        if ((ch == ')' && chSt == '(') || (ch == '}' && chSt == '{') || (ch == ']' && chSt == '['))
+                            continue;
+
+                        else return false;
+                    }
+                    else return false;
+                }
+                else stack.Push(ch);
+            }
+            return stack.Count > 0 ? false : true;
         }
     }
 }
