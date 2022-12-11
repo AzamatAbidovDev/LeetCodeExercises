0941737db114f806b0031b48dd6c4575ea4e56b0
admire114su@gmail.com <admire114su@gmail.com>
2022-12-11 17:12:45 +0500

    Ex5

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maximum Value of a String in an Array
+            string[] text = new string[] { "ascscd", "yv", "22", "c", "5001", "928", "4003", "2" };
+            int maxVal = MaximumValue(text);
+            Console.WriteLine(maxVal);
+
+            Console.ReadLine();
+        }
+
+        //Maximum Value of a String in an Array
+        static int MaximumValue(string[] strs)
+        {
+            int value = 0;            
+            List<int> values = new List<int>();
+
+            foreach (string str in strs)
+            {
+                foreach (char item in str)
+                {
+                    if (Char.IsLetter(item))
+                    {
+                        value = str.Length;
+                        break;
+                    }
+                }
+                if(value == 0)
+                {
+                    value = int.Parse(string.Join("", str.Where(c => char.IsDigit(c))));
+                }
+
+                values.Add(value);
+                value = 0;
+            }
+            return values.Max();
+        }
+    }
+}
