34c09e59c55e78a5328c68fc0e6615443d47afe6
admire114su@gmail.com <admire114su@gmail.com>
2022-12-12 22:31:18 +0500

    Ex7

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,6 +13,20 @@ namespace ConsoleApp1
             int maxVal = MaximumValue(text);
             Console.WriteLine(maxVal);
 
+            //Climbing Stairs
+            maxVal = ClimbStairs(5);
+            Console.WriteLine(maxVal);
+
+            //Merge Sorted Array
+            int[] numbers1 = new int[5] { 1,3,2,0,0};
+            int[] numbers2 = new int[2] { 6,4};
+            Merge(numbers1, 3, numbers2, 2);
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                Console.Write(numbers1[i]);
+            }
+            Console.WriteLine();
+
             Console.ReadLine();
         }
 
@@ -41,6 +55,43 @@ namespace ConsoleApp1
                 value = 0;
             }
             return values.Max();
+        }
+
+        //Sqrt(x)
+        static int MySqrt(int x)
+        {
+            double result = Math.Sqrt(x);
+            return (int)result;
+        }
+        //Pow(x, n)
+        static double MyPow(double x, int n)
+        {
+            return Math.Pow(x, n);
+        }
+
+        //Climbing Stairs
+        static int ClimbStairs(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+
+            return ClimbStairs(n - 1) + ClimbStairs(n - 2);
+        }
+
+        //Merge Sorted Array
+        static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m; i < nums1.Length; i++)
+            {
+                if (nums1[i] == 0)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        nums1[i] = nums2[j];
+                        i++;
+                    }
+                }
+            }
+            Array.Sort(nums1);
         }
     }
 }
